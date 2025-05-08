830c43e7073d660a5ae4d67ee72b3faf42e1a8c6
PzDarby <izliquihd@gmail.com>
2025-05-08 09:30:54 +0200

    Añadidas las clases de funciones y main

---
+++ Reto3GermanJuan/src/util/SqlConnection.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SqlConnection {
+	private static String url = "jdbc:mysql://localhost:3306/instituto";
+	private static String user = "newuser";
+	private static String passwd = "izquierdo";
+	private static Connection con;
+	
+	public static Connection abirConexion() {
+		try {
+			con = DriverManager.getConnection(url, user, passwd);
+			return con;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public static void cierraConexion() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
